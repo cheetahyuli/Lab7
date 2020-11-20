fdb26b688241bf11060b44a2348c65fb9e9999ee
cheetahyuli <cheetahyuli@gmail.com>
2020-11-20 22:55:50 +0800

    first

--- Lab7_2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lab7_2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity
         tv_weight = findViewById(R.id.tv_weight);
         tv_bmi = findViewById(R.id.tv_bmi);
         tv_progress = findViewById(R.id.tv_progress);
-        tv_progress = findViewById(R.id.ll_progress);
+        ll_progress = findViewById(R.id.ll_progress);
         progressBar2 = findViewById(R.id.progressBar2);
 
         findViewById(R.id.btn_calculate).setOnClickListener(new View.OnClickListener()
@@ -41,17 +41,17 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                if(ed_height.length() < 1)
+                if (ed_height.length() < 1)
                     Toast.makeText(MainActivity.this,
                             "請輸入身高", Toast.LENGTH_SHORT).show();
-                else if(ed_weight.length() < 1)
+                else if (ed_weight.length() < 1)
                     Toast.makeText(MainActivity.this,
                             "請輸入體重", Toast.LENGTH_SHORT).show();
                 else
                     runAsyncTask();
             }
         });
-
+    }
         @SuppressLint("StaticFieldLeak")
         private void runAsyncTask()
         {
@@ -69,7 +69,61 @@ public class MainActivity extends AppCompatActivity
 
                     ll_progress.setVisibility(View.VISIBLE);
                 }
-            }
+
+                @Override
+                protected Boolean doInBackground(Void... voids)
+                {
+                    int progress = 0;
+
+                    while(progress <= 100)
+                    {
+                        try
+                        {
+                            Thread.sleep(50);
+
+                            publishProgress(progress);
+
+                            progress++;
+                        }catch (InterruptedException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                    return true;
+                }
+
+                @Override
+                protected void onProgressUpdate(Integer... values)
+                {
+                    super.onProgressUpdate(values);
+
+                    progressBar2.setProgress(values[0]);
+                    tv_progress.setText(values[0] + "%");
+                }
+
+                @Override
+                protected void onPostExecute(Boolean aBoolean)
+                {
+                    super.onPostExecute(aBoolean);
+                    ll_progress.setVisibility(View.GONE);
+
+                    int h = Integer.valueOf(ed_height.getText().toString());
+                    int w = Integer.valueOf(ed_weight.getText().toString());
+                    double standWeight, bodyFat;
+                    if(btn_boy.isChecked())
+                    {
+                        standWeight = (h - 80) * 0.7;
+                        bodyFat = (w - 0.88 * standWeight)/w * 100;
+                    }
+                    else
+                    {
+                        standWeight = (h - 70) * 0.6;
+                        bodyFat = (w - 0.82 * standWeight)/w * 100;
+                    }
+
+                    tv_weight.setText(String.format("標準體重\n%.2f", standWeight));
+                    tv_bmi.setText(String.format("體脂肪\n%.2f", bodyFat));
+                }
+            }.execute();
         }
-    }
 }
